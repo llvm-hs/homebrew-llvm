d747c17e19bd1dd584d0c31c6fd17b3bd782b052
Trevor L. McDonell <trevor.mcdonell@gmail.com>
2021-05-10 16:34:13 +0200

    add llvm-11

---
+++ Formula/llvm-11.rb
@@ -0,0 +1,172 @@
+class Llvm11 < Formula
+  desc "Next-gen compiler infrastructure"
+  homepage "http://llvm.org/"
+
+  version = "11.1.0"
+
+  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-#{version}/llvm-project-#{version}.src.tar.xz"
+  sha256 "74d2529159fd118c3eac6f90107b5611bccc6f647fdea104024183e8d5e25831"
+
+  bottle do
+    root_url "https://github.com/llvm-hs/homebrew-llvm/releases/download/v11.1.0"
+    sha256 cellar :any, big_sur: ""
+  end
+
+  # http://releases.llvm.org/11.0.0/docs/GettingStarted.html#requirements
+  depends_on "cmake" => :build
+
+  uses_from_macos "libedit"
+  uses_from_macos "libffi", since: catalina
+  uses_from_macos "libxml2"
+  uses_from_macos "ncurses"
+  uses_from_macos "zlib"
+
+  # version suffix
+  def ver
+    "11"
+  end
+
+  # http://releases.llvm.org/11.0.0/docs/CMake.html
+  def install
+    # Apple's libstdc++ is too old to build LLVM
+    ENV.libcxx if ENV.compiler == :clang
+
+    # compiler-rt has some iOS simulator features that require i386 symbols. I'm
+    # assuming the rest of clang also needs support for 32-bit compilation to
+    # work correctly, but if not, perhaps universal binaries could be limited to
+    # compiler-rt. LLVM makes this somewhat easier because compiler-rt can
+    # almost be treated as an entirely different build from LLVM.
+    ENV.permit_arch_flags
+
+    projects = %w[
+      clang
+      clang-tools-extra
+      lld
+      lldb
+      openmp
+      polly
+      mlir
+    ]
+    runtimes = %w[
+      compiler-rt
+      libcxx
+      libcxxabi
+      libunwind
+    ]
+
+    clang_buildpath  = buildpath/"tools/clang"
+    libcxx_buildpath = buildpath/"projects/libcxx"
+
+    clang_buildpath.install resource("clang")
+    libcxx_buildpath.install resource("libcxx")
+    (buildpath/"tools/lld").install resource("lld")
+    (buildpath/"tools/polly").install resource("polly")
+    (buildpath/"tools/clang/tools/extra").install resource("clang-tools-extra")
+    (buildpath/"projects/openmp").install resource("openmp")
+    (buildpath/"projects/libunwind").install resource("libunwind")
+    (buildpath/"projects/compiler-rt").install resource("compiler-rt")
+
+    install_prefix = lib/"llvm-#{ver}"
+
+    args = %W[
+      -DCMAKE_BUILD_TYPE=Release
+      -DCMAKE_INSTALL_PREFIX=#{install_prefix}
+      -DLLVM_ENABLE_PROJECTS=#{projects.join(";")}
+      -DLLVM_ENABLE_RUNTIMES=#{runtimes.join(";")}
+      -DLLVM_ENABLE_ASSERTIONS=ON
+      -DLLVM_OPTIMIZED_TABLEGEN=ON
+      -DLLVM_INCLUDE_DOCS=OFF
+      -DLLVM_INCLUDE_TESTS=OFF
+      -DLLVM_ENABLE_RTTI=ON
+      -DLLVM_ENABLE_EH=ON
+      -DLLVM_INSTALL_UTILS=ON
+      -DLLVM_ENABLE_Z3_SOLVER=OFF
+      -DWITH_POLLY=ON
+      -DLINK_POLLY_INTO_TOOLS=ON
+      -DLLVM_TARGETS_TO_BUILD=all
+      -DLIBOMP_ARCH=x86_64
+      -DLLVM_BUILD_EXTERNAL_COMPILER_RT=ON
+      -DLLVM_BUILD_LLVM_C_DYLIB=ON
+      -DLLVM_LINK_LLVM_DYLIB=ON
+      -DLLVM_ENABLE_LIBCXX=ON
+      -DLLVM_ENABLE_FFI=ON
+      -DLLVM_CREATE_XCODE_TOOLCHAIN=ON
+      -DLLVM_CREATE_XCODE_TOOLCHAIN=#{MacOS::Xcode.installed? ? "ON" : "OFF"}
+      -DRUNTIMES_CMAKE_ARGS="-DCMAKE_INSTALL_RPATH=@loader_path/../lib"
+    ]
+
+    if MacOS.version >= :catalina
+      args << "-DFFI_INCLUDE_DIR=#{MacOS.sdk_path}/usr/include/ffi"
+      args << "-DFFI_LIBRARY_DIR=#{MacOS.sdk_path}/usr/lib"
+    else
+      args << "-DFFI_INCLUDE_DIR=#{Formula["libffi"].opt_include}"
+      args << "-DFFI_LIBRARY_DIR=#{Formula["libffi"].opt_lib}"
+    end
+
+    mkdir "build" do
+      system "cmake", "-G", "Unix Makefiles", "..", *(std_cmake_args + args)
+      system "cmake", "--build", "."
+      system "cmake", "--build", ".", "--target", "install"
+      system "cmake", "--build", ".", "--target", "install-xcode-toolchain" if MacOS::Xcode.installed?
+    end
+
+    (share/"clang-#{ver}/tools").install Dir["tools/clang/tools/scan-{build,view}"]
+    inreplace share/"clang-#{ver}/tools/scan-build/bin/scan-build", "$RealBin/bin/clang", install_prefix/"bin/clang"
+    (install_prefix/"bin").install_symlink share/"clang-#{ver}/tools/scan-view/bin/scan-view"
+    (install_prefix/"bin").install_symlink share/"clang-#{ver}/tools/scan-build/bin/scan-build"
+    (install_prefix/"share/man/man1").install_symlink share/"clang-#{ver}/tools/scan-build/scan-build.1"
+
+    (lib/"python2.7/site-packages").install "bindings/python/llvm" => "llvm-#{ver}",
+                                            clang_buildpath/"bindings/python/clang" => "clang-#{ver}"
+
+    # replace the existing "clang -> clang-11" symlink
+    rm install_prefix/"bin/clang"
+    mv install_prefix/"bin/clang-#{ver}", install_prefix/"bin/clang"
+
+    # These versioned .dylib symlinks are missing for some reason
+    # Note that we use relative symlinks
+    ln_s "libLLVM.dylib", install_prefix/"lib/libLLVM-#{ver}.dylib"
+
+    # Set LC_LOAD_DYLIB entries to absolute paths
+    system "install_name_tool", "-change", "@rpath/libLLVM.dylib", install_prefix/"lib/libLLVM.dylib", install_prefix/"lib/libLTO.dylib"
+    system "install_name_tool", "-change", "@rpath/libLLVM.dylib", install_prefix/"lib/libLLVM.dylib", install_prefix/"lib/libclang.dylib"
+
+    # Set LC_ID_DYLIB entries to absolute paths
+    system "install_name_tool", "-id", install_prefix/"lib/libLLVM.dylib", install_prefix/"lib/libLLVM.dylib"
+    system "install_name_tool", "-id", install_prefix/"lib/libLTO.dylib", install_prefix/"lib/libLTO.dylib"
+    system "install_name_tool", "-id", install_prefix/"lib/libc++.1.0.dylib", install_prefix/"lib/libc++.1.0.dylib"
+    system "install_name_tool", "-id", install_prefix/"lib/libclang.dylib", install_prefix/"lib/libclang.dylib"
+    system "install_name_tool", "-id", install_prefix/"lib/libomp.dylib", install_prefix/"lib/libomp.dylib"
+    system "install_name_tool", "-id", install_prefix/"lib/libunwind.1.0.dylib", install_prefix/"lib/libunwind.1.0.dylib"
+
+    Dir.glob(install_prefix/"bin/*") do |exec_path|
+      basename = File.basename(exec_path)
+      bin.install_symlink exec_path => "#{basename}-#{ver}"
+    end
+
+    Dir.glob(install_prefix/"share/man/man1/*") do |manpage|
+      basename = File.basename(manpage, ".1")
+      man1.install_symlink manpage => "#{basename}-#{ver}.1"
+    end
+  end
+
+  def caveats; <<~EOS
+    Extra tools are installed in #{opt_share}/clang-#{ver}
+
+    To link to libc++, something like the following is required:
+      CXX="clang++-#{ver} -stdlib=libc++"
+      CXXFLAGS="$CXXFLAGS -nostdinc++ -I#{opt_lib}/llvm-#{ver}/include/c++/v1"
+      LDFLAGS="$LDFLAGS -L#{opt_lib}/llvm-#{ver}/lib"
+    EOS
+  end
+
+  test do
+    assert_equal prefix.to_s, shell_output("#{bin}/llvm-config-#{ver} --prefix").chomp
+
+    # test for sed errors since some llvm makefiles assume that sed
+    # understands '\n' which is true for gnu sed and not for bsd sed.
+    assert_no_match /PATH\)n/, (lib/"llvm-#{ver}/share/llvm/cmake/LLVMConfig.cmake").read
+    system "#{bin}/llvm-config-#{ver}", "--version"
+  end
+end
+
